c03fc56ba8fefc1dde766175e87d669edf9bf9f9
Fiona McDonald <fiona.m.mcdonald@gmail.com>
2014-07-31 17:56:54 +0100

    Member model TDD

--- app/models/Member.scala
+++ app/models/Member.scala
@@ -9,10 +9,12 @@ import scala.slick.driver.H2Driver.simple._
  */
 
 
-case class Member(id: Option[Int] = None, firstName: String)
+case class Member(id: Option[Int] = None, firstName: String, lastName: String, membershipNumber: Int)
 
-class Members(tag: Tag) extends Table[Member](tag, "MODEL") {
+class Members(tag: Tag) extends Table[Member](tag, "MEMBER") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def firstName = column[String]("FIRST_NAME")
-  def * = (id.?, firstName) <> (Member.tupled, Member.unapply _)
+  def lastName = column[String]("LAST_NAME")
+  def membershipNumber = column[Int]("MEMBERSHIP_NUMBER")
+  def * = (id.?, firstName, lastName, membershipNumber) <> (Member.tupled, Member.unapply _)
 }
--- test/models/MemberSpec.scala
+++ test/models/MemberSpec.scala
@@ -5,7 +5,7 @@ import scala.slick.driver.H2Driver.simple._
 import scala.slick.jdbc.meta._
 import org.h2.jdbc.JdbcSQLException
 
-class MemberSpec extends FunSpec with BeforeAndAfter {
+class MemberSpec extends FunSpec with BeforeAndAfter with Matchers {
   
   implicit var session: Session = _
   
@@ -16,11 +16,28 @@ class MemberSpec extends FunSpec with BeforeAndAfter {
     members.ddl.create
   }
   
+  after {
+    session.close()
+  }
+  
   describe("Validation") {
     it("should validate the presence of firstName") {
       intercept[JdbcSQLException] {
-        members += new Member(None, null)
+        members += new Member(None, null, "Brown", 1370)
       }
+    }
+    
+    it("should validate the presence of lastName") {
+      intercept[JdbcSQLException] {
+        members += new Member(None, "Bobby", null, 1370)
+      }
+    }
+  }
+  
+  describe("Save") {
+    it("should create an ID") {
+      val memberId = (members returning members.map(_.id)) += Member(None, "Bobby", "Brown", 1370)
+      memberId should be > 0
     }
   }
 
